4214693faf7dced5b8f57e9e6bc7b8672dfa2464
Julius Dejon <dejonjulz@gmail.com>
2019-01-20 12:07:53 +0800

    Updates

---
+++ precache-manifest.d0418f3be1fb824869f5dde2cfa4710d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25da0c3d6c95f141b2f9",
+    "url": "/react-task-kiler/static/js/runtime~main.25da0c3d.js"
+  },
+  {
+    "revision": "b8e453be175d71e1296f",
+    "url": "/react-task-kiler/static/js/main.b8e453be.chunk.js"
+  },
+  {
+    "revision": "c5716c9e8711f39be066",
+    "url": "/react-task-kiler/static/js/1.c5716c9e.chunk.js"
+  },
+  {
+    "revision": "b8e453be175d71e1296f",
+    "url": "/react-task-kiler/static/css/main.fa6b97b4.chunk.css"
+  },
+  {
+    "revision": "b00b5f41d9c51de8bf6833d4221aad41",
+    "url": "/react-task-kiler/index.html"
+  }
+];
