1517f3148d37b5695fd74651c19ef51712cd40e0
Julius Dejon <dejonjulz@gmail.com>
2019-01-20 10:47:52 +0800

    now can delete (just filtering) array accessed on props passed by Hi component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+import './App.css'
 
-import Table from './Table';
+import Table from './Table'
 
 class App extends Component {
-  render() {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor'
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer'
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress'
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender'
+      }
+    ]
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      })
+    })
+  }
+
+  render () {
+    const { characters } = this.state
     return (
-      <div className="container">
-        <Table />
+      <div className='container'>
+        <Container fluid>
+          <Table
+            characterData={characters}
+            removeCharacter={this.removeCharacter}
+          />
+        </Container>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Table.js
+++ src/Table.js
@@ -1,66 +1,57 @@
-import React from 'react'
-import { Header, Image, Table } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Header, Image, Table, Button } from 'semantic-ui-react'
 
-const TableExampleCollapsing = () => (
-  <Table basic='very' celled collapsing>
+const TableHeader = () => {
+  return (
     <Table.Header>
       <Table.Row>
         <Table.HeaderCell>Employee</Table.HeaderCell>
-        <Table.HeaderCell>Correct Guesses</Table.HeaderCell>
+        <Table.HeaderCell>Action</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
+  )
+}
+const TableBody = props => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <Table.Row key={index}>
+        <Table.Cell>
+          <Header as='h4' image>
+            <Image
+              src='https://react.semantic-ui.com/images/avatar/small/matthew.png'
+              rounded
+              size='mini'
+            />
+            <Header.Content>
+              {row.name}
+              <Header.Subheader>{row.job}</Header.Subheader>
+            </Header.Content>
+          </Header>
+        </Table.Cell>
+        <Table.Cell>
+          <Button negative onClick={() => props.removeCharacter(index)}>
+            Delete
+          </Button>
+        </Table.Cell>
+      </Table.Row>
+    )
+  })
+  return <Table.Body>{rows}</Table.Body>
+}
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h4' image>
-            <Image src='https://react.semantic-ui.com/images/avatar/small/lena.png' rounded size='mini' />
-            <Header.Content>
-              Lena
-              <Header.Subheader>Human Resources</Header.Subheader>
-            </Header.Content>
-          </Header>
-        </Table.Cell>
-        <Table.Cell>22</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h4' image>
-            <Image src='https://react.semantic-ui.com/images/avatar/small/matthew.png' rounded size='mini' />
-            <Header.Content>
-              Matthew
-              <Header.Subheader>Fabric Design</Header.Subheader>
-            </Header.Content>
-          </Header>
-        </Table.Cell>
-        <Table.Cell>15</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h4' image>
-            <Image src='https://react.semantic-ui.com/images/avatar/small/lindsay.png' rounded size='mini' />
-            <Header.Content>
-              Lindsay
-              <Header.Subheader>Entertainment</Header.Subheader>
-            </Header.Content>
-          </Header>
-        </Table.Cell>
-        <Table.Cell>12</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h4' image>
-            <Image src='https://react.semantic-ui.com/images/avatar/small/mark.png' rounded size='mini' />
-            <Header.Content>
-              Mark
-              <Header.Subheader>Executive</Header.Subheader>
-            </Header.Content>
-          </Header>
-        </Table.Cell>
-        <Table.Cell>11</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-)
+class DefaultTable extends Component {
+  render () {
+    const { characterData, removeCharacter } = this.props
+    return (
+      <Table basic='very' celled collapsing>
+        <TableHeader />
+        <TableBody
+          characterData={characterData}
+          removeCharacter={removeCharacter}
+        />
+      </Table>
+    )
+  }
+}
 
-export default TableExampleCollapsing
+export default DefaultTable
